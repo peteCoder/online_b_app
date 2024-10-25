0d6d799de4baa66b771c3bdf81ef4dc58f7cb7cb
peteCoder <petercodercoder@gmail.com>
2024-10-25 09:33:13 -0700

    added remote db

--- online_b_f/settings.py
+++ online_b_f/settings.py
@@ -131,18 +131,18 @@ WSGI_APPLICATION = 'online_b_f.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config("DATABASE_URL")
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
+}
 
 
 
